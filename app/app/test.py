6a10a54104cc4d9acdaf1ecf5ea036fc38676786
Sushovit204 <shahsushovit434@gmail.com>
2023-04-29 21:26:31 +0545

    Set up Postgres in Docker

---
+++ app/app/test.py
@@ -0,0 +1,11 @@
+"""Django test module"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5,4)
+
+        self.assertEqual(res,9)
