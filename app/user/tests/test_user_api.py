98e8a9815a0b6caa243a734e55a85b6ee0e6705b
Sushovit204 <shahsushovit434@gmail.com>
2023-05-18 22:02:28 +0545

    Implementing create user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -46,9 +46,9 @@ class PublicUserApiTests(TestCase):
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
-        
+
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        
+
     def test_password_too_short_error(self):
         """Test an error is returned if password is short than  5 chars."""
         payload = {
@@ -61,6 +61,5 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
-        ).exists
+        ).exists()
         self.assertFalse(user_exists)
-        
